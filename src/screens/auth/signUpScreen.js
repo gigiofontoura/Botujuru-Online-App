52aedf70ded5086319dbf899bf094334a8fff0b3
gi <giovannasantosfontoura@gmail.com>
2026-06-09 18:51:00 -0300

    telas de cadastro e pesquisa adicionadas

--- src/screens/auth/signUpScreen.js
+++ src/screens/auth/signUpScreen.js
@@ -1,10 +1,43 @@
-import { View, Text } from "react-native";
+import {View, Text, TextInput, StyleSheet} from "react-native"
+import {GreenButton} from "../../components/Button"
+import { Input } from "../../components/Input"
 
-export default function SignUpScreen(){
+
+export default function SignUpScreen({navigation}){
     return(
-        <View>
-            <Text>CADASTRAR</Text>
+        <View style={styles.container}>
+
+            <View style={styles.headerContainer}>
+            <Text style={styles.textHeader}>Realize seu cadastro</Text>
+            </View>
+
+        <View style={styles.form}>
+            <Input placeholder= "Digite seu email"></Input>
+            <Input placeholder= "Crie uma senha"></Input>
+            <Input placeholder= "Repita sua senha"></Input>
+
+
         </View>
+
+        <GreenButton title="Completar Cadastro" onPress={() => navigation.replace("MainApp")}></GreenButton>
+
+        </View>
+
+
     )
 };
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f5f5f5",
+        gap: 40,
+        alignItems: "center",
+        marginTop: 160,
+    },
+
+    form:{
+        gap: 35,
+
+    }
+})
--- src/screens/barraNavegacaoInferior/searchScreen.js
+++ src/screens/barraNavegacaoInferior/searchScreen.js
@@ -1,13 +1,81 @@
-import { View, Text } from "react-native";
-import Header from "../../components/Header";
-import BottomTab from "../../components/ButtonTab"; 
+import {View, Text, TextInput, StyleSheet, TouchableOpacity, Image} from "react-native"
+import { Input } from "../../components/Input"
+import { Feather } from "@expo/vector-icons";
+import { Title } from "react-native-paper";
 
-export default function SearchScreen({navigation}){
+export default function SearchScreen(){
     return(
-        <View style={{flex: 1}}>
-            <Header></Header>
-            <BottomTab navigation={navigation}></BottomTab>
+        <View style={styles.container}>
+           <View style={styles.header}>
+
+        <View style={styles.searchContainer}>
+            <Feather
+                name="search"
+                size={20}
+                color="gray"
+            />
+
+            <TextInput placeholder="Pesquisar" style={styles.input}/>
+
+        </View>
+
+        <Text style={styles.Title}>
+            Última Notícia
+
+        </Text>
+
+        <View style={styles.News}>
+            <Image source={require("../../../assets/saude1.png")} style={styles.newsImage}/>
+            <Text style={styles.Noticia}>Campo Paulista recebe nova ambulância do governo estadual.</Text>
+
+        </View>
+
+
+           </View>
         </View>
     )
 };
 
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: "#F5F5F5",
+        paddingTop: 70, 
+        paddingHorizontal: 25,
+        alignItems: "center",
+    },
+        searchContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        width: 220,
+        height: 45,
+        borderWidth: 1,
+        borderColor: "#DDD",
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        backgroundColor: "white",
+    },
+
+    Title: {
+        marginTop: 5,
+        fontSize: 22,
+        fontWeight: "bold",
+        fontFamily: "inter",
+        color: "#4D9F55"
+    },
+
+    header:{
+        gap: 20,
+    },
+
+    Noticia:{
+        fontFamily: "inter",
+        fontSize: 15,
+        gap: 10,
+        paddingTop: 20,
+        
+    },
+
+  
+})
